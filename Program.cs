f2bcea0b6b0928c3dd8601c8b96773e099903013
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-12-11 11:35:51 -0700

    working with arrays

--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;  // Import the System namespace
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var numbers = new int[3];
+            numbers[0] = 1;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[2]);
+            Console.WriteLine(flags[3]);
+
+            var names = new string[3] { "jack", "John", "Cena" };
+        }
+    }
+}
 
